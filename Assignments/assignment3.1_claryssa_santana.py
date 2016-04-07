2582709e939e3928d500fcca5043840cd35afe4b
claryssa <cs4416@nyu.edu>
2016-04-07 19:26:14 -0400

    Assignment 3

---
+++ Assignments/assignment3.1_claryssa_santana.py
@@ -0,0 +1,21 @@
+''' Claryssa Santana 
+	Assignment 3 
+	Purpose: Create a payment calculator for laborer that also calculates over time ''' 
+
+hours = raw_input("Enter Hours: ")
+rate = raw_input("Enter Pay Rate: ")
+
+try:
+	hours = float(hours)
+	rate = float(rate)
+
+	if hours <= 40.0 : 
+		normalPay = hours * rate
+	if hours > 40.0 : 
+		overtime = hours * (rate/2) 
+	pay = normalPay + overtime
+	print "Total Salary: ", pay
+	 
+except: 
+	print "Incorrect value entered"
+
