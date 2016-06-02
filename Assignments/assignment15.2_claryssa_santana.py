4c59ec9e71233a50ad2cd77b82d85a685d538c2d
Claryssa <claryssa.santana@gmail.com>
2016-06-02 17:30:33 -0400

    Assignment 15.2

--- Assignments/assignment15.2_claryssa_santana.py
+++ Assignments/assignment15.2_claryssa_santana.py
@@ -2,52 +2,22 @@
 Data Programming with Python
 Assignment 15.2
 31 May 2016
-Purpose: Prompt for a URL, read the JSON data from that URL using urlliband then parse and 
-extract the comment counts from the JSON data then compute the sum of the numbers in the file.
+Purpose: Prompt for a URL, read the JSON data from that URL using urlliband 
+then parse and extract the comment counts from the JSON data then compute the 
+sum of the numbers in the file.
 '''
 import urllib
-import xml.etree.ElementTree as ET
 import json
 
-serviceurl = 'http://maps.googleapis.com/maps/api/geocode/xml?'
-
 while True:
-    address = raw_input('Enter location: ')
-    if len(address) < 1 : break
+	url = raw_input('\nEnter location: ')
+	if len(url) < 1 : break
 
-    url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
-    print 'Retrieving', url
-    uh = urllib.urlopen(url)
-    data = uh.read()
-    print 'Retrieved',len(data),'characters'
-    print data
-    tree = ET.fromstring(data)
+	print 'Retrieving', url
+	print '\nRetrieved',len(urllib.urlopen(url).read()),'characters'
+	print 'Count:', len(json.loads(urllib.urlopen(url).read())['comments'])
 
-
-    results = tree.findall('result')
-    lat = results[0].find('geometry').find('location').find('lat').text
-    lng = results[0].find('geometry').find('location').find('lng').text
-    location = results[0].find('formatted_address').text
-
-    print 'lat',lat,'lng',lng
-    print location
-
-input = '''
-[
-  { "id" : "001",
-    "x" : "2",
-    "name" : "Chuck"
-  } ,
-  { "id" : "009",
-    "x" : "7",
-    "name" : "Chuck"
-  } 
-]'''
-
-info = json.loads(input)
-print 'User count:', len(info)
-
-for item in info:
-    print 'Name', item['name']
-    print 'Id', item['id']
-    print 'Attribute', item['x']
+	total = 0 
+	for num in json.loads(urllib.urlopen(url).read())['comments']:
+		total += int(num['count'])
+	print 'Sum:', total
